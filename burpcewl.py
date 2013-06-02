e62f11976c62d73ff121a05d034d31ce5ce2da13
Joseph N. Wilson <jnw@cise.ufl.edu>
2013-06-01 23:58:09 -0400

    Basic functionality achieved. Now parses html docs using ElementSoup

--- burpcewl.py
+++ burpcewl.py
@@ -12,6 +12,8 @@ from optparse import OptionParser
 from BaseHTTPServer import BaseHTTPRequestHandler
 from StringIO import StringIO
 from httplib import HTTPResponse
+import ElementSoup
+import re
 
 class StringSocket(StringIO):
     ''' StringSocket
@@ -22,13 +24,12 @@ class StringSocket(StringIO):
     def makefile(self, *args, **kw):
         return self
 
-def httpparse(str):
+def http_parse(str):
     socket = StringSocket(str)
     response = HTTPResponse(socket)
     response.begin()
-
     return response
-    
+
 class HTTPRequest(BaseHTTPRequestHandler):
     '''HTTPRequest
     provides code necessary to parse an HTTPRequest
@@ -49,18 +50,45 @@ class HTTPRequest(BaseHTTPRequestHandler):
 class Namespace: pass
 
 def vprint(arg):
-    if globals.VERBOSE:
+    if OPTIONS.VERBOSE:
         print arg
+    
+def get_tag_content(str,tag):
+            begin_index = str.find('<' + tag + '>')
+            if begin_index < 0:
+                raise LookupError
+            end_index = str.find('</' + tag + '>')
+            if end_index < 0:
+                raise LookupError
+            return (str[end_index + len(tag) + 3:],
+                        str[begin_index + len(tag) + 2 : end_index])
+
+def remove_CDATA(str):
+    CDATA_START = '<![CDATA['
+    CDATA_END = ']]>'
+    if str.find(CDATA_START) == 0:
+        if str[-3:] == CDATA_END:
+            str = str[len(CDATA_START):-len(CDATA_END)]
+        else:
+            raise LookupError
+    return str
+
+def html_get_words(str):
+    tree = ElementSoup.parse(StringIO(str))   
+    for text in tree.itertext():
+        for word in re.sub("[^\w]", " ",  text).split():
+            if len(word) >= OPTIONS.min_word_length:
+                try:
+                    DICTIONARY[word] = DICTIONARY[word] + 1
+                except:
+                    DICTIONARY[word] = 1
+def do_pass(str):
+    pass
 
 def main():
-    REQUEST_OPEN_TAG = '<request>'
-    REQUEST_CLOSE_TAG = '</request>'
-    RESPONSE_OPEN_TAG = '<response>'
-    RESPONSE_CLOSE_TAG = '</response>'
-    
     usage = ("%prog [options] burp-session-file\n"
-             "  minese a burp session file for possible passwords\n"
-             "  motiviated by digininja's cewl")
+             "  mine a burp session file for possible passwords\n"
+             "  motiviated by digininja's cewl\n")
     parser = OptionParser()
     parser.add_option("-d","--depth",
                       action="store_const", const=2, dest="depth",
@@ -95,38 +123,67 @@ def main():
     parser.add_option("--meta-temp-dir",
                       action="store", dest="meta_temp_dir",
                       help="temporary directory used by exiftool when parsing file, default /tmp")
-    (options,args) = parser.parse_args()
 
-    global globals
-    globals = Namespace()
-    globals.VERBOSE = options.VERBOSE
-    
+    global OPTIONS
+    global DICTIONARY
+
+    (OPTIONS,args) = parser.parse_args()
+    DICTIONARY = {}
+
     if len(args) != 1:
         parser.print_usage()
     else:
         vprint("Converting burp session file to xml")
-        burp_xml_string = burp_to_xml(args[0])
+        try:
+            burp_xml_str = burp_to_xml(args[0],True)
+        except:
+            print 'burp2xml.py failed to parse session file'
+            raise
+
+        action_map = {
+            'application/pdf':do_pass,
+            'application/x-gzip':do_pass,
+            'application/x-shockwave-flash':do_pass,
+            'audio/x-wav':do_pass,
+            'image/jpeg':do_pass,
+            'image/gif':do_pass,
+            'image/png':do_pass,
+            'image/x-bitmap':do_pass,
+            'text/css':do_pass,
+            'text/html':html_get_words,
+            'text/plain':do_pass,
+        }
 
         # First Get the Request
         while 1:
-            rq_begin_index = burp_xml_string.find(REQUEST_OPEN_TAG)
-            if rq_begin_index < 0:
+            try:
+                (burp_xml_str, rq_str) = get_tag_content(burp_xml_str,'request')
+                request = HTTPRequest(rq_str)
+  #              print request.headers['Host'] + request.path
+            except LookupError:
                 break
-            rq_end_index = burp_xml_string.find(REQUEST_CLOSE_TAG)
-            if rq_end_index < 0:
-                # throw exception for bad formatting
-                break
-            request_string = burp_xml_string[rq_begin_index +
-                                             len(REQUEST_OPEN_TAG):
-                                             rq_end_index]
-            
-            request = HTTPRequest(request_string)
-            print request.headers['Host'] + request.path
-            
-            burp_xml_string = burp_xml_string[rq_end_index +
-                                              len(REQUEST_CLOSE_TAG):]
-            
+            try:
+                (burp_xml_str, rsp_str) = get_tag_content(burp_xml_str,'response')
+                rsp_str = remove_CDATA(rsp_str)
+                response = http_parse(rsp_str)
+                type_str = response.getheader('Content-Type')
+                semi_index = type_str.find(';')
+                if semi_index >= 0:
+                    type_str = type_str[0:semi_index].lower()
+#                print ' ' + type_str
+                body_str = response.read()
+                try:
+                    action_map[type_str](body_str)
+                except:
+                    pass
 
+
+            except LookupError:
+ #               print "Missing response"
+                raise 
+
+        for word in sorted(DICTIONARY.keys()):
+            print word + ':' + str(DICTIONARY[word])
     
 if __name__ == '__main__':
     main()
