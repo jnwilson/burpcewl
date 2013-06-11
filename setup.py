1dae969bb4f554eae02f3cc2853a64283d7715d6
Joseph N. Wilson <jnw@cise.ufl.edu>
2013-06-11 13:37:52 -0400

    install_requires dependencies added

--- setup.py
+++ setup.py
@@ -10,15 +10,14 @@ setup(name='burpcewl',
       license='BSD',
       url='https://github.com/jnwilson/burpcewl',
       py_modules=['burpcewl'],
-      requires=[
+      install_requires=[
           'burp2xml',
           'lxml',
           'magic',
        ],
       dependency_links=[
-          'https://github.com/jnwilson/burp2xml#egg=burp2xml',
-          'https://github.com/lxml/lxml.git#egg=lxml',
-          'https://github.com/ahupp/python-magic.git#egg=python-magic',
+          'https://github.com/jnwilson/burp2xml/tarball/master#egg=burp2xml',
+          'https://github.com/ahupp/python-magic/tarball/master#egg=python-magic',
         ],
       classifiers=[
           'Development Status :: 3 - Alpha',
