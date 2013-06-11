b25eecab9f1e3cd6432ef14160e0b57f882e354b
Joseph N. Wilson <jnw@cise.ufl.edu>
2013-06-11 00:06:13 -0400

    setup fixed (slightly)

--- setup.py
+++ setup.py
@@ -11,18 +11,17 @@ setup(name='burpcewl',
       url='https://github.com/jnwilson/burpcewl',
       py_modules=['burpcewl'],
       requires=[
-          'burp2xml>=1.1',
+          'burp2xml',
           'lxml',
-          'python-magic',
+          'magic',
        ],
       dependency_links=[
           'https://github.com/jnwilson/burp2xml#egg=burp2xml',
           'https://github.com/lxml/lxml.git#egg=lxml',
-          'https://github.com/ahupp/python-magic.git',
+          'https://github.com/ahupp/python-magic.git#egg=python-magic',
         ],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'License :: OSI Approved :: BSD License',
-          'Topic :: Security',
       ],
 )
